8d25a748e50afa405c9fcad30c308d88f0cc03d8
Ruslan Sayfutdinov <sayfutdinov.ruslan@icloud.com>
2023-02-10 09:58:41 +0300

    1.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,108 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './app/index.js',
-    module: {
-        rules: [
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-            { test: /\.(js)$/, use: 'babel-loader' }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-}
+  entry: './src/index.ts',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+      {
+        test: /\.scss|css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|svg)$/i,
+        type: 'asset/resource',
+        include: [
+          path.resolve(__dirname, 'src/fonts')
+        ],
+        generator: {
+          filename: path.join('assets/fonts', '[name][ext]'),
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        include: [
+          path.resolve(__dirname, 'src/images')
+        ],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        include: [
+          path.resolve(__dirname, 'src/images')
+        ],
+        generator: {
+          filename: path.join('assets/icons', '[name][ext]'),
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    assetModuleFilename: path.join('assets/images', '[name][ext]')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.pug'),
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ['build'],
+        }
+      },
+    }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 8080
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-mozjpeg",
+              "imagemin-pngquant",
+              "imagemin-svgo",
+            ],
+          },
+        },
+        generator: [
+          {
+            preset: "webp",
+            implementation: ImageMinimizerPlugin.imageminGenerate,
+            options: {
+              plugins: ["imagemin-webp"],
+            },
+          },
+        ],
+      }),
+    ],
+  },
+};
